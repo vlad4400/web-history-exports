2bea4b0a8c33e9345e1efa25aba63bb4f4a02124
vlad4400 <sleepvs@gmail.com>
2021-07-27 09:37:13 +0200

    create project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatePageComponent } from './create-page/create-page.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'dashboard', component: DashboardPageComponent},
+  {path: 'create', component: CreatePageComponent},
+  {path: 'post/:id/edit', component: EditPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { CreatePageComponent } from './create-page/create-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardPageComponent,
+    CreatePageComponent,
+    EditPageComponent,
+    LoginPageComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
